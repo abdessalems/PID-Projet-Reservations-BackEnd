48a3640b2c16128893d4c0b069e333e4cc6eeda9
SAADAOUI ABDESSALEM <abdessalemsaa@gmail.com>
2024-09-25 23:32:47 +0200

    Initial commit

---
+++ src/main/java/com/saadaoui/reservations/ReservationsApplication.java
@@ -0,0 +1,13 @@
+package com.saadaoui.reservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/saadaoui/reservations/ReservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saadaoui.reservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
